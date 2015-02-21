e3ab9d0629b821aeaf6e4c6e7efbe1e4f5ac1650
Loïc Haas <loic@haas.link>
2015-02-22 00:14:02 +0100

    Implement instrument needed in TrumpetTest.java

---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author zoruk
+ */
+class Flute implements IInstrument {
+
+    public Flute() {
+    }
+
+    @Override
+    public String play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSoundVolume() {
+        return 1;
+    }
+
+    @Override
+    public String getColor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author zoruk
+ */
+class Trumpet implements IInstrument {
+
+    public Trumpet() {
+    }
+
+    @Override
+    public String play() {
+        return "pouet";
+    }
+
+    @Override
+    public int getSoundVolume() {
+        return 2;
+    }
+
+    @Override
+    public String getColor() {
+        return "golden";
+    }
+}
